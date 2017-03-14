a59c27f62e61ffd3c90eca39a249c4d977c5eb3d
Michelbaartman <michelbaartman@gmail.com>
2017-03-14 16:42:40 +0100

    v2.6

--- dodgegame/dodgegame/dodgegame/main.c
+++ dodgegame/dodgegame/dodgegame/main.c
@@ -1,26 +1,40 @@
 #include <wiringPi.h>
 #include <stdio.h>
 #include <wiringShift.h>
+#include <stdbool.h>
 
 int main(int argc, char **argv)
 {
+    // layout led matrix
     wiringPiSetupGpio();
-    int ledMatrix[2][4]= {
-        {5,6,13,19},
-        {12,16,20,21}};
+    int ledMatrix[2][4][2] = {
+        {{5,false},{6,false},{13,false},{19,false}},
+        {{12,false},{16,false},{20,false},{21,false}}};
     int i = 0;
     int j = 0;
     
+    // set led matrix
     for(i; i < 2; i++){
         for(j = 0; j < 4; j++){
-            pinMode(ledMatrix[i][j], OUTPUT);
-            digitalWrite(ledMatrix[i][j], HIGH);
+            pinMode(ledMatrix[i][j][0], OUTPUT);
+            digitalWrite(ledMatrix[i][j][0], HIGH);
             delay(100);
-            digitalWrite(ledMatrix[i][j], LOW);
+            digitalWrite(ledMatrix[i][j][0], LOW);
             delay(100);
         }
     }
     
+    // block sequences
+    int blockSequence[1][2][4] = {
+        {1, 0, 0, 0}, {0, 0, 1, 1}
+    }
+    
+    int currentSequence[2][8] = {
+        {0, 1, 0, 1, 0, 0, 0, 0}, 
+        {0, 0, 0, 1, 0, 1, 0, 0}
+    }
+    
+    // setup player controls
     int button1 = 26;
     int button2 = 22;
     pinMode(button1, INPUT);
@@ -28,37 +42,37 @@ int main(int argc, char **argv)
     pinMode(button2, INPUT);
     pullUpDnControl(button2, PUD_UP);
     
-    int player = ledMatrix[0][3];
+    // player setup
+    int player = ledMatrix[0][3][0];
     
+    // block setup
     int blockX = 0;
     int blockY = 0;
-    int block = ledMatrix[blockX][blockY];
+    int block = ledMatrix[blockX][blockY][0];
     digitalWrite(player,1);
     digitalWrite(block, 1);
     
+    // game loop
     int count = 0;
     int speed = 50;
     while(1){
-        if( count > speed){
-            digitalWrite(block, 0);
-            blockY++;
-            if(blockY > 3){
-                blockY = -1;
+        // track movement
+        if(count > speed){
+            for(int i = 0; i < 8; i++){
             }
-            block = ledMatrix[blockX][blockY];
-            digitalWrite(block, 1);
-            count = 0;
-            speed --;
         }
+        
+        // player input
         if(digitalRead(button1)){
             digitalWrite(player, 0);
-            player = ledMatrix[1][3];
+            player = ledMatrix[1][3][0];
             digitalWrite(player, 1);
         }else if(digitalRead(button2)){
             digitalWrite(player,0);
-            player = ledMatrix[0][3];
+            player = ledMatrix[0][3][0];
             digitalWrite(player,1);
         }
+        
         count++;
         delay(5);
     }
