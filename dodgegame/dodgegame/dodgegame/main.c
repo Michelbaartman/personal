0d0e3e9bf75f76da570c82ee651138a24c9b0be2
Michelbaartman <michelbaartman@gmail.com>
2017-03-16 12:37:07 +0100

    7

--- dodgegame/dodgegame/dodgegame/main.c
+++ dodgegame/dodgegame/dodgegame/main.c
@@ -3,7 +3,7 @@
 #include <wiringShift.h>
 #include <stdbool.h>
 
-#delare ledMatrix[2][4][2] = { { {5,false},{6,false},{13,false},{19,false} }, { {12,false},{16,false},{20,false},{21,false} } };
+int ledMatrix[2][4][2] = { { {5,false},{6,false},{13,false},{19,false} }, { {12,false},{16,false},{20,false},{21,false} } };
 
 void led_switch(int ledX, int ledY, char command) // s = switch, + = on, - = off
 {
